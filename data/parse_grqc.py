11146fed5e21af11be8a789f7c6d1fede155a27a
Tao Lin <nblintao@gmail.com>
2017-04-16 12:02:02 -0400

    Use larger dataset and implemented a parser for sampling

---
+++ data/parse_grqc.py
@@ -0,0 +1,24 @@
+import json
+with file('ca-GrQc.txt', 'r') as text:
+    all_lines = text.readlines()
+    all_lines = all_lines[5:]
+    n_all_edge = len(all_lines)
+    print('All edges: %d' % n_all_edge)
+    n_edge = input('Sample edges: ')
+    if n_edge == 0: n_edge = n_all_edge
+    graph = {'nodes':[], 'links':[]}
+    nodes = set()
+    for i in range(0, n_edge):
+        line = all_lines[i]
+        src, tar = line.strip().split()
+        link = {'source': src, 'target': tar, 'value': 1}
+        graph['links'].append(link)
+        nodes.add(src)
+        nodes.add(tar)
+    for node in nodes:
+        node_info = {'id': node, 'group':1}
+        graph['nodes'].append(node_info)
+    output = 'var graph = ' + json.dumps(graph)
+    with file('grqc_auto.js', 'w') as out:
+        out.write(output)
+
