c693a21e95e78ca0d2f22387797bda82bb618b2d
Bowei Chen <boweichen@BoweideMacBook.local>
2017-05-08 21:13:34 -0400

    Add interface for paragraphl.js

---
+++ sigma.layout.paragraphl.js
@@ -0,0 +1,33 @@
+;(function(undefined) {
+
+  if (typeof sigma == 'undefined')
+    throw new Error('sigma is not declared');
+
+  // Initialize : 
+  sigma.utils.pkg('sigma.layouts.paragraphl');
+
+  var _instance = {};
+
+  var _eventEmitter = {};
+
+  function ParagraphL() {
+    var self = this;
+
+    // Configuration
+    this.init = function (sigInst, options) {
+
+    };
+
+    // Layout algorithm implementation
+    this.singleStep = function () {
+
+    };
+
+    // Start the layout algorithm 
+    this.start = function() {
+
+    };
+
+  }
+  
+}).call(this);
