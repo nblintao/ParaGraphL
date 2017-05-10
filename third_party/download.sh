a883a33d271e2eeeeff9d916f9e31e7e6d49ca8c
Bowei Chen <boweichen@BoweideMacBook.wv.cc.cmu.edu>
2017-05-09 21:03:11 -0400

    Comment unused download scripts

--- third_party/download.sh
+++ third_party/download.sh
@@ -4,7 +4,8 @@ npm install
 npm run build
 cd ..
 
-npm install -g node-gyp
-npm i node-deps-opengl-raub
-sudo xcode-select --switch /Applications/Xcode.app/Contents/Developer
+# npm install -g node-gyp
+# npm i node-deps-opengl-raub
+## Xcode version 8.2 with Apple LLVM 8.0 is needed for Mac OS
+#sudo xcode-select --switch /Applications/Xcode.app/Contents/Developer
 
